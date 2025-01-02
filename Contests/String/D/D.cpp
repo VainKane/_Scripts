4edbef0d5adb7110e3dc861c615bddf9e81719dd
Vain_Kane <vidueduo@gmail.com>
2025-01-02 23:39:54 +0700

    2/1/2025

--- Contests/String/D/D.cpp
+++ Contests/String/D/D.cpp
@@ -4,16 +4,53 @@ using namespace std;
 
 string str;
 
-int res;
+
+int n;
+
+int res = 1;
+
+bool s[(int)1e4][(int)1e4];
 
 
 int main()
 {
-    getline(cin, str);
 
+     getline(cin, str);
 
+     n = str.length();
 
-    cout << res;
+     for (int i = 0; i < n; i++)
+     {
+         s[i][i] = true;
+     }
+
+     for (int i = 0; i < n; i++)
+     {
+         for (int j = 1; j < n; j++)
+         {
+             if (j - i == 1)
+             {
+                 if (str[i] == str[j])
+                 {
+                     s[i][j] = true;
+                 }
+             }
+             else
+             {
+                 if (str[i] == str[j] && s[i + 1][j - 1])
+                 {
+                     s[i][j] = true;
+
+                     if (j - i + 1 > res)
+                     {
+                         res = j - i + 1;
+                     }
+                 }
+             }
+         }
+     }
+
+     cout << res;
 
     return 0;
 }
