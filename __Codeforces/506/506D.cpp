1d7fa4290154ae101dc52341be3febc0e65fc758
Vain_Kane <vidueduo@gmail.com>
2026-03-14 20:00:42 +0700

    14/3/2026

--- __Codeforces/506/506D.cpp
+++ __Codeforces/506/506D.cpp
@@ -22,10 +22,127 @@ template <class t> bool mini(t &x, t const &y)
     return x > y ? x = y, 1 : 0;
 }
 
+struct DSU
+{
+    vector<int> par;
+    vector<int> sz;
+    int n;
+
+    void MakeSet()
+    {
+        FOR(i, 1, n)
+        {
+            par[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    DSU(int _n = 0)
+    {
+        n = _n;
+        par.assign(n + 5, 0);
+        sz.assign(n + 5, 0);
+        MakeSet();
+    }
+
+    int Find(int v)
+    {
+        if (par[v] == v) return v;
+        return par[v] = Find(par[v]);
+    }
+
+    void Union(int a, int b)
+    {
+        a = Find(a);
+        b = Find(b);
+
+        if (a == b) return;
+
+        if (sz[a] < sz[b]) swap(a, b);
+        par[b] = a;
+        sz[a] += sz[b];
+    }
+};
+
 int const N = 2e5 + 5;
+int const BK = 314;
+int const GR = N / BK + 5;
 
 int n, m;
 vector<pair<int, int>> edges[N];
+
+int hvId[N];
+int hv = 0;
+
+DSU dsu[GR];
+
+vector<int> adj[N];
+unordered_map<int, int> cnt[N];
+
+bool visited[N];
+vector<int> ccV[N];
+int cc = 0;
+
+void BFS(int s)
+{
+    queue<int> q;
+    q.push(s);
+
+    visited[s] = true;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        ccV[cc].push_back(u);
+
+        for (auto &v : adj[u]) if (!visited[v])
+        {
+            visited[v] = true;
+            q.push(v);
+        }
+    }
+}
+
+void Init()
+{
+    FOR(i, 1, m) 
+    {
+        if (sz(edges[i]) > BK) 
+        {
+            hvId[i] = ++hv;
+            dsu[hv] = DSU(n);
+            for (auto &e : edges[i]) dsu[hv].Union(e.F, e.S);
+        }
+        else
+        {
+            for (auto &e : edges[i])
+            {
+                adj[e.F].push_back(e.S);
+                adj[e.S].push_back(e.F);
+            }
+
+            for (auto &e : edges[i])
+            {
+                if (!visited[e.F]) cc++, BFS(e.F);
+                if (!visited[e.S]) cc++, BFS(e.S);
+            }
+
+            FOR(id, 1, cc) for (auto &u : ccV[id]) for (auto &v : ccV[id]) cnt[u][v]++;
+
+            for (auto &e : edges[i])
+            {
+                visited[e.F] = visited[e.S] = false;
+                adj[e.F].clear();
+                adj[e.S].clear();
+            }
+
+            FOR(i, 1, cc) ccV[i].clear();
+            cc = 0;
+        }
+    }
+}
 
 int main()
 {
@@ -40,10 +157,17 @@ int main()
         edges[c].push_back({u, v});
     }
 
+    Init();
+
     int q; cin >> q;
     while (q--)
     {
         int u, v;
+        cin >> u >> v;
+
+        int res = cnt[u][v];
+        FOR(i, 1, hv) res += dsu[i].Find(u) == dsu[i].Find(v);
+        cout << res << '\n';
     }
 
     return 0;
