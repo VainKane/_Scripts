74dbc609d7126c39d01a11741ff08f0d925717c6
Vain_Kane <vidueduo@gmail.com>
2026-03-30 21:46:14 +0700

    30/3/2026

---
+++ __Codeforces/Gym/Grader&OutputOnly(373765)/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include "aplusb.h"
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; i--)
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define BIT(i, x) (((x) >> (i)) & 1)
+#define MK(i) (1LL << (i))
+#define all(v) v.begin(), v.end()
+#define sz(v) ((int)v.size())
+#define F first
+#define S second
+
+template <class t> bool maxi(t &x, t const &y)
+{
+    return x < y ? x = y, 1 : 0;
+}
+
+template <class t> bool mini(t &x, t const &y)
+{
+    return x > y ? x = y, 1 : 0;
+}
+
+int sum_ab(int a, int b)
+{
+    return a + b;
+}
